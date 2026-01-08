be30035493c59e721380d76a3ef2a5f931ecf6f8
SimuStruct <alyssonbarbosa_@outlook.com>
2026-01-08 19:26:13 -0300

    Update __init__.py.

---
+++ SimuFrame/GUI/__init__.py
@@ -0,0 +1,13 @@
+"""
+SimuFrame.GUI - Graphical User Interface module.
+
+Finite element analysis for frame structures with geometric nonlinearity
+using Total Lagrangian formulation and both Newton-Raphson and Arc-Length method.
+
+Author: Alysson Barbosa
+Created: 2023-12-25
+Last Modified: 2026-01-04
+"""
+
+__version__ = "0.3.1"
+__author__ = "Alysson Barbosa"
